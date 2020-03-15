69c665c9e21c615f95f51ed2a9c4ac6dec4b634b
Yashasvi Sriram <yash.3997@gmail.com>
2020-03-15 00:57:33 -0500

    makes RRT demos that are identical to RRT* demos

--- src/demos/RRT2.java
+++ src/demos/RRT2.java
@@ -12,15 +12,15 @@ import tools.rrt.RapidlyExploringRandomTree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RRT extends PApplet {
+public class RRT2 extends PApplet {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
     public static final int SIDE = 100;
     final Vec3 minCorner = Vec3.of(0, -SIDE, -SIDE);
     final Vec3 maxCorner = Vec3.of(0, SIDE, SIDE);
 
-    final Vec3 startPosition = Vec3.of(0, SIDE * (9f / 10), SIDE * (-9f / 10));
-    final Vec3 finishPosition = Vec3.of(0, SIDE * (-9f / 10), SIDE * (9f / 10));
+    final Vec3 startPosition = Vec3.of(0, SIDE * 0.9f, -SIDE * 0.9f);
+    final Vec3 finishPosition = Vec3.of(0, -SIDE * 0.9f, SIDE * 0.9f);
     SphericalAgentDescription sphericalAgentDescription;
     SphericalAgent sphericalAgent;
     List<SphericalObstacle> sphericalObstacles = new ArrayList<>();
@@ -43,12 +43,38 @@ public class RRT extends PApplet {
         noStroke();
 
         cam = new QueasyCam(this);
-        sphericalObstacles.add(new SphericalObstacle(
-                this,
-                Vec3.of(0, 0, 0),
-                SIDE * (2f / 20),
-                Vec3.of(1, 0, 1)
-        ));
+        for (int i = 0; i < 9; i++) {
+            sphericalObstacles.add(new SphericalObstacle(
+                    this,
+                    Vec3.of(0, SIDE * 0.9f - 20, SIDE * -0.9f + 20 * i - 10),
+                    SIDE * 0.1f,
+                    Vec3.of(1, 0, 1)
+            ));
+        }
+        for (int i = 0; i < 9; i++) {
+            sphericalObstacles.add(new SphericalObstacle(
+                    this,
+                    Vec3.of(0, SIDE * 0.9f - 60, SIDE * -0.5f + 20 * i - 10),
+                    SIDE * 0.1f,
+                    Vec3.of(1, 0, 1)
+            ));
+        }
+        for (int i = 0; i < 9; i++) {
+            sphericalObstacles.add(new SphericalObstacle(
+                    this,
+                    Vec3.of(0, SIDE * -0.9f + 20, SIDE * -0.5f + 20 * i - 10),
+                    SIDE * 0.1f,
+                    Vec3.of(1, 0, 1)
+            ));
+        }
+        for (int i = 0; i < 9; i++) {
+            sphericalObstacles.add(new SphericalObstacle(
+                    this,
+                    Vec3.of(0, SIDE * -0.9f + 60, SIDE * -0.9f + 20 * i - 10),
+                    SIDE * 0.1f,
+                    Vec3.of(1, 0, 1)
+            ));
+        }
         sphericalAgentDescription = new SphericalAgentDescription(
                 startPosition,
                 SIDE * (0.5f / 20)
@@ -117,7 +143,7 @@ public class RRT extends PApplet {
     }
 
     static public void main(String[] passedArgs) {
-        String[] appletArgs = new String[]{"demos.RRT"};
+        String[] appletArgs = new String[]{"demos.RRT2"};
         if (passedArgs != null) {
             PApplet.main(concat(appletArgs, passedArgs));
         } else {
