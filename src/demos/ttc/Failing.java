52638c3730e2d67476d6826e85a26ba20e76bd09
Yashasvi Sriram <yash.3997@gmail.com>
2020-03-27 21:45:41 -0500

    renames ttc zig zag as failing

--- src/demos/ttc/Failing.java
+++ src/demos/ttc/Failing.java
@@ -14,7 +14,7 @@ import robot.sensing.PlainConfigurationSpace;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ZigZag extends PApplet {
+public class Failing extends PApplet {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
     public static final int SIDE = 100;
@@ -160,7 +160,7 @@ public class ZigZag extends PApplet {
     }
 
     static public void main(String[] passedArgs) {
-        String[] appletArgs = new String[]{"demos.ttc.ZigZag"};
+        String[] appletArgs = new String[]{"demos.ttc.Failing"};
         if (passedArgs != null) {
             PApplet.main(concat(appletArgs, passedArgs));
         } else {
